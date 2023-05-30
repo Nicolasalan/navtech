45c6bfc29557978b282d04de0a1fd4a9c79b586b
Nicolas Alan <nicolasalangrotti03@gmail.com>
2023-05-30 00:14:05 -0300

    create pages

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -18,27 +18,61 @@ export default {
     // Sidebar
     sidebar: [
       {
-        text: "Purposes",
+        text: "Introdução e Visão Geral do Robô",
         collapsible: true,
         items: [
-          { text: "Introduction", link: "/introduction" },
-          { text: "Getting Started", link: "/getting-started" },
+          { text: "Descrição do robô, sua finalidade e aplicações", link: "/pages/introduction/description.md" },
+          { text: "Visão geral dos componentes eletrônicos, mecânicos e de software", link: "/pages/introduction/vision.md" },
+          { text: "Requisitos e especificações do sistema", link: "/pages/introduction/requirements.md" },
         ],
       },
       {
-        text: "Hardware",
+        text: "Componentes Eletrônicos",
         collapsible: true,
         items: [
-          { text: "Introduction", link: "/introduction" },
-          { text: "Getting Started", link: "/getting-started" },
+          { text: "Lista de componentes eletrônicos utilizados no robô", link: "/pages/electronics/list.md" },
+          { text: "Diagramas de circuito", link: "/pages/electronics/diagrams.md" },
+          { text: "Especificações dos componentes e funções", link: "/pages/electronics/specifications.md" },
+          { text: "Instruções de montagem e conexão dos componentes", link: "/pages/electronics/instructions.md" },
         ],
       },
       {
-        text: "Software",
+        text: "Estrutura Mecânica",
         collapsible: false,
         items: [
-          { text: "Introduction", link: "/introduction" },
-          { text: "Getting Started", link: "/getting-started" },
+          { text: "Introduction", link: "/pages/mechanics/introduction.md" },
+          { text: "Materiais utilizados na construção", link: "/pages/mechanics/materials.md" },
+          { text: "Projetos CAD e desenhos técnicos", link: "/pages/mechanics/designs.md" },
+          { text: "Instruções de montagem e configuração mecânica", link: "/pages/mechanics/assembly.md" },
+        ],
+      },
+      {
+        text: "Software e Ambiente ROS",
+        collapsible: false,
+        items: [
+          { text: "Introdução ao ROS (Robot Operating System) e sua importância", link: "/pages/software/introduction.md" },
+          { text: "Configuração do ambiente de desenvolvimento ROS", link: "/pages/software/settings.md" },
+          { text: "Integração com o pacote Nav2 para navegação autônoma", link: "/pages/software/integration.md" },
+          { text: "Descrição dos pacotes ROS utilizados no robô", link: "/pages/software/description.md" },
+        ],
+      },
+      {
+        text: "Configuração e Parametrização",
+        collapsible: false,
+        items: [
+          { text: "Configuração do ambiente ROS e pacotes relacionados", link: "/pages/parameterization/environment.md" },
+          { text: "Configuração de sensores e atuadores", link: "/pages/parameterization/sensors.md" },
+          { text: "Parametrização dos algoritmos de navegação e planejamento de trajetória", link: "/pages/parameterization/algorithms.md" },
+        ],
+      },
+      {
+        text: "Operação e Manutenção",
+        collapsible: false,
+        items: [
+          { text: "Instruções de inicialização e desligamento do robô", link: "/pages/maintenance/instructions.md" },
+          { text: "Guia de operação para usuários finais", link: "/pages/maintenance/guide.md" },
+          { text: "Procedimentos de manutenção preventiva e corretiva", link: "/pages/maintenance/procedures.md" },
+          { text: "Solução de problemas e resolução de problemas comuns", link: "/pages/maintenance/solution.md" },
         ],
       },
     ],
--- src/navtech/robot/launch/navigation_launch.py
+++ src/navtech/robot/launch/navigation_launch.py
@@ -13,6 +13,7 @@ def generate_launch_description():
     # Get the launch directory
     bringup_dir = get_package_share_directory('robot')
 
+    # parameters to pass to the nav2 stack
     namespace = LaunchConfiguration('namespace')
     use_sim_time = LaunchConfiguration('use_sim_time')
     autostart = LaunchConfiguration('autostart')
@@ -20,6 +21,7 @@ def generate_launch_description():
     default_bt_xml_filename = LaunchConfiguration('default_bt_xml_filename')
     map_subscribe_transient_local = LaunchConfiguration('map_subscribe_transient_local')
 
+    # nodes to launch
     lifecycle_nodes = ['controller_server',
                        'planner_server',
                        'recoveries_server',
