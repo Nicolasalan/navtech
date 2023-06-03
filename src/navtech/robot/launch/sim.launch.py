cd1af6f40abfc01b73c5629d8ed49adc0dac63cc
Nicolasalan <nicolasalangrotti03@gmail.com>
2023-06-02 22:22:27 -0300

    update sim launch

--- src/navtech/robot/launch/sim.launch.py
+++ src/navtech/robot/launch/sim.launch.py
@@ -26,13 +26,14 @@ def generate_launch_description():
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
-             )
+    )
 
     # Run the spawner node from the gazebo_ros package. The entity name doesn't really matter if you only have a single robot.
     spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
                         arguments=['-topic', 'robot_description',
                                    '-entity', 'robot'],
-                        output='screen')
+                        output='screen'
+    )
     
     # Launch them all!
     return LaunchDescription([
